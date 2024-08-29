55a25bfc581230de4a4ba27c65b57df30af41138
Joe Lyles <lylesjoe@protonmail.com>
2024-08-29 06:36:55 -0400

    added templates and login form

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,36 @@
+from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, World"
+  user = {'username': 'Joe'}
+  posts = [
+    {
+      'author': {'username': 'John'},
+      'body': 'Beautify day in Amarillo today. Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+    },
+    {
+      'author': {'username': 'Ande'},
+      'body': 'I should have used a Blackwing.'
+    },
+    {
+      'author': {'username': 'Ron'},
+      'body': 'Platform 9 & 3/4!'
+    },
+    {
+      'author': {'username': 'Gandalf'},
+      'body': 'There are no safe paths in this part of the world, Bilbo.'
+    }
+  ]
+  return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/about')
+def about():
+  return render_template('about.html', title="about")
+
+@app.route('/login')
+def login():
+  form = LoginForm()
+  return render_template('login.html', title='Sign In', form=form)
