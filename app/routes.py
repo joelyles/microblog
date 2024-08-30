e791ddd61885d592237624b8513c10b895fe242e
Joe Lyles <lylesjoe@protonmail.com>
2024-08-30 05:59:01 -0400

    post method, error msgs, and sql model

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -30,7 +30,12 @@ def index():
 def about():
   return render_template('about.html', title="about")
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
   form = LoginForm()
+  if form.validate_on_submit():
+    flash('Login requested for user {}, remember_me={}'.format(
+      form.username.data, form.remember_me.data
+    ))
+    return redirect(url_for('index'))
   return render_template('login.html', title='Sign In', form=form)
